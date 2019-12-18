ba685ff1f3098abee4b3a5b265332578038ae617
Pablo Arranz Alés <parranz@ayesa.com>
2019-12-18 21:26:34 +0100

    Conexion mogodb atlas

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,7 @@ process.env.NODE_ENV =  process.env.NODE_ENV || 'dev1';
 // ===============================
 let urlDB;
 
+process.env.MONGO_URI = 'mongodb+srv://cafe-user:cafe123456@clusterstarter0-pq2ea.azure.mongodb.net/cafe'
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
